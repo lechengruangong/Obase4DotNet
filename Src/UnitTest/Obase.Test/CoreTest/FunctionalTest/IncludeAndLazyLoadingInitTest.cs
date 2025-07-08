086c1f8e5bda61e11962eb2d4f4d1e4d23fca69e
Athloniux <athlon6062@gmail.com>
2025-07-08 11:24:42 +0800

    增加包含和延迟加载测试 增加复用上下文测试

--- Src/UnitTest/Obase.Test/CoreTest/FunctionalTest/IncludeAndLazyLoadingInitTest.cs
+++ Src/UnitTest/Obase.Test/CoreTest/FunctionalTest/IncludeAndLazyLoadingInitTest.cs
@@ -1,4 +1,8 @@
-﻿using Obase.Test.Configuration;
+﻿using System.Linq;
+using Obase.Core;
+using Obase.Providers.Sql;
+using Obase.Test.Configuration;
+using Obase.Test.Domain.Association;
 
 namespace Obase.Test.CoreTest.FunctionalTest;
 
@@ -18,6 +22,41 @@ public class IncludeAndLazyLoadingInitTest
         {
             var context = ContextUtils.CreateContext(dataSource);
             //清理可能的冗余数据
+            context.CreateSet<Student>().Delete(p => p.StudentId > 0);
+            context.CreateSet<Class>().Delete(p => p.ClassId > 0);
+            context.CreateSet<Teacher>().Delete(p => p.TeacherId > 0);
+
+            //添加一个教师
+            var teacher = new Teacher
+            {
+                Name = "延迟加载无通行证教师"
+            };
+
+            context.Attach(teacher);
+            context.SaveChanges();
+
+            //添加班级和学生
+            //学校的班级
+            var newclass = new Class
+            {
+                Name = "初始化容器某某班"
+            };
+
+            context.Attach(newclass);
+
+            //加入学生
+            for (var i = 1; i < 6; i++)
+            {
+                var student = new Student
+                {
+                    Class = newclass,
+                    Name = $"小{i}"
+                };
+                context.Attach(student);
+            }
+
+            //保存
+            context.SaveChanges();
         }
     }
 
@@ -31,6 +70,59 @@ public class IncludeAndLazyLoadingInitTest
         {
             var context = ContextUtils.CreateContext(dataSource);
             //清理可能的冗余数据
+            context.CreateSet<Student>().Delete(p => p.StudentId > 0);
+            context.CreateSet<Class>().Delete(p => p.ClassId > 0);
+            context.CreateSet<Teacher>().Delete(p => p.TeacherId > 0);
         }
+    }
+
+    /// <summary>
+    ///     测试包含和延迟加载初始化容器
+    /// </summary>
+    [TestCaseSource(typeof(TestCaseSourceConfigurationManager),
+        nameof(TestCaseSourceConfigurationManager.DataSourceTestCases))]
+    public void Test(EDataSource dataSource)
+    {
+        var context = ContextUtils.CreateContext(dataSource);
+        //此教师无通行证
+        var queryTeacher = context.CreateSet<Teacher>().FirstOrDefault(p => p.Name == "延迟加载无通行证教师");
+        //使用延迟加载进行加载 获得一个空容器
+        Assert.That(queryTeacher, Is.Not.Null);
+        Assert.That(queryTeacher.PassPaperList, Is.Not.Null);
+        Assert.That(queryTeacher.PassPaperList.Count, Is.EqualTo(0));
+
+        context = ContextUtils.CreateContext(dataSource);
+        //使用Include进行加载 获得一个空容器
+        queryTeacher = context.CreateSet<Teacher>().Include(p => p.PassPaperList)
+            .FirstOrDefault(p => p.Name == "延迟加载无通行证教师");
+
+        Assert.That(queryTeacher, Is.Not.Null);
+        Assert.That(queryTeacher.PassPaperList, Is.Not.Null);
+        Assert.That(queryTeacher.PassPaperList.Count, Is.EqualTo(0));
+
+        //删除
+        context.Remove(queryTeacher);
+        context.SaveChanges();
+
+        //测试无延迟加载的关联
+        context = ContextUtils.CreateContext(dataSource);
+
+        var clazz = context.CreateSet<Class>().FirstOrDefault();
+        //没有包含Student 是空值
+        Assert.That(clazz, Is.Not.Null);
+        Assert.That(clazz.Students, Is.Null);
+        //没有包含ClassTeachers 是空值
+        Assert.That(clazz, Is.Not.Null);
+        Assert.That(clazz.ClassTeachers, Is.Null);
+
+        clazz = context.CreateSet<Class>().Include(p => p.Students).Include(p => p.ClassTeachers)
+            .FirstOrDefault(p => p.Name == "初始化容器某某班");
+        Assert.That(clazz, Is.Not.Null);
+        //加载了Student 有值
+        Assert.That(clazz.Students, Is.Not.Null);
+        Assert.That(clazz.Students.Count, Is.EqualTo(5));
+        //加载了ClassTeacher 没值
+        Assert.That(clazz.ClassTeachers, Is.Not.Null);
+        Assert.That(clazz.ClassTeachers.Count, Is.EqualTo(0));
     }
 }
--- Src/UnitTest/Obase.Test/CoreTest/FunctionalTest/MultiplexContextTest.cs
+++ Src/UnitTest/Obase.Test/CoreTest/FunctionalTest/MultiplexContextTest.cs
@@ -1,4 +1,8 @@
-﻿using Obase.Test.Configuration;
+﻿using System;
+using System.Linq;
+using Obase.Providers.Sql;
+using Obase.Test.Configuration;
+using Obase.Test.Domain.SimpleType;
 
 namespace Obase.Test.CoreTest.FunctionalTest;
 
@@ -18,6 +22,29 @@ public class MultiplexContextTest
         {
             var context = ContextUtils.CreateContext(dataSource);
             //清理可能的冗余数据
+            context.CreateSet<JavaBean>().Delete(p => p.IntNumber > 0);
+
+            //添加新对象
+            for (var i = 1; i < 21; i++)
+                context.CreateSet<JavaBean>().Attach(new JavaBean
+                {
+                    Bool = i % 2 == 0,
+                    DateTime = DateTime.Now,
+                    DecimalNumber = new decimal(Math.Pow(Math.PI, i)),
+                    IntNumber = i,
+                    LongNumber = i,
+                    ByteNumber = (byte)i,
+                    CharNumber = '\u006A',
+                    FloatNumber = (float)Math.Pow(Math.PI, i),
+                    DoubleNumber = (float)Math.Pow(Math.PI, i),
+                    Time = new TimeSpan(0, 8, 40, 0),
+                    Date = DateTime.Now,
+                    String = $"{i}号字符串",
+                    Guid = Guid.NewGuid(),
+                    Strings = new[] { $"{i - 1}", $"{i}", $"{i + 1}" }
+                });
+
+            context.SaveChanges();
         }
     }
 
@@ -31,6 +58,41 @@ public class MultiplexContextTest
         {
             var context = ContextUtils.CreateContext(dataSource);
             //清理可能的冗余数据
+            context.CreateSet<JavaBean>().Delete(p => p.IntNumber > 0);
         }
+    }
+
+    /// <summary>
+    ///     测试复用上下文
+    /// </summary>
+    [TestCaseSource(typeof(TestCaseSourceConfigurationManager),
+        nameof(TestCaseSourceConfigurationManager.DataSourceTestCases))]
+    public void MultiplexTest(EDataSource dataSource)
+    {
+        var context = ContextUtils.CreateContext(dataSource);
+        //随意查询一部分
+        var querySet = context.CreateSet<JavaBean>().Where(p => !p.Bool).ToList();
+
+        Assert.That(querySet, Is.Not.Null);
+        Assert.That(querySet.Count, Is.EqualTo(10));
+
+        //修改部分数据
+        for (var i = 0; i < querySet.Count; i++)
+            if (i % 2 == 0)
+                querySet[i].String = $"{querySet[i].String}_{i++}";
+
+        context.SaveChanges();
+        //查出另外一部分数据 修改
+        querySet = context.CreateSet<JavaBean>().Where(p => !p.Bool && p.String.EndsWith("2")).ToList();
+        Assert.That(querySet, Is.Not.Null);
+        Assert.That(querySet.Count, Is.EqualTo(1));
+
+        foreach (var javaBean in querySet) javaBean.DecimalNumber++;
+
+        context.SaveChanges();
+        //查询所有数据
+        querySet = context.CreateSet<JavaBean>().ToList();
+        Assert.That(querySet, Is.Not.Null);
+        Assert.That(querySet.Count, Is.EqualTo(20));
     }
 }
