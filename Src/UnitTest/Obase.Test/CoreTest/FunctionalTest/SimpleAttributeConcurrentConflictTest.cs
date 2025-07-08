533d4225466898435f2bdc5b5d112021b6eb1ecc
Athloniux <athlon6062@gmail.com>
2025-07-08 11:39:53 +0800

    增加查看执行的Sql语句测试

--- Src/UnitTest/Obase.Test/CoreTest/FunctionalTest/SimpleAttributeConcurrentConflictTest.cs
+++ Src/UnitTest/Obase.Test/CoreTest/FunctionalTest/SimpleAttributeConcurrentConflictTest.cs
@@ -1,4 +1,7 @@
-﻿namespace Obase.Test.CoreTest.FunctionalTest;
+﻿using Obase.Test.Configuration;
+using Obase.Test.Domain.Functional;
+
+namespace Obase.Test.CoreTest.FunctionalTest;
 
 /// <summary>
 ///     简单属性测试并发冲突策略
@@ -6,4 +9,32 @@
 [TestFixture]
 public class SimpleAttributeConcurrentConflictTest
 {
+    /// <summary>
+    ///     构造实例 为上下文赋值
+    /// </summary>
+    [OneTimeSetUp]
+    public void SetUp()
+    {
+        foreach (var dataSource in TestCaseSourceConfigurationManager.DataSources)
+        {
+            var context = ContextUtils.CreateContext(dataSource);
+            //都是一个表 清理一次即可
+            context.CreateSet<IngoreKeyValue>().Delete(p => p.Id > 0);
+        }
+    }
+
+
+    /// <summary>
+    ///     销毁对象
+    /// </summary>
+    [OneTimeTearDown]
+    public void Dispose()
+    {
+        foreach (var dataSource in TestCaseSourceConfigurationManager.DataSources)
+        {
+            var context = ContextUtils.CreateContext(dataSource);
+            //都是一个表 清理一次即可
+            context.CreateSet<IngoreKeyValue>().Delete(p => p.Id > 0);
+        }
+    }
 }
