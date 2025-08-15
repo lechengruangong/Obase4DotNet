32a1f5dbfa7fcb0d617b507c693641b816c3baec
Decima <athlon6062@gmail.com>
2025-08-15 09:17:25 +0800

    合并6.4.1的改动 (#15)
    * 增加已存在连接的事务执行器和测试
    
    * 修改ExistingConnectionSqlExecutor判断本地事务是否开启的方法
    
    * 增加基于已有连接的SQL执行器回滚的逻辑
    
    * 已存在连接的事务测试中增加连接提供方使用同一个事务但Obase执行的部分发生异常导致回滚的测试
    
    * 调整已存在连接的上下文注释
    
    * 版本提升至6.4.1
    
    * 修正修改记录标号错误

---
+++ Src/UnitTest/Obase.Test.Infrastructure/Context/PostgreSqlExistingConnectionContext.cs
@@ -0,0 +1,52 @@
+﻿using System.Data.Common;
+using Obase.Core;
+using Obase.Core.Odm.Builder;
+using Obase.Providers.Sql;
+using Obase.Test.Infrastructure.ModelRegister;
+
+namespace Obase.Test.Infrastructure.Context;
+
+/// <summary>
+///     使用使用已存在的PostgreSql连接上下文
+/// </summary>
+public class PostgreSqlExistingConnectionContext : ObjectContext
+{
+    /// <summary>
+    ///     构造ObjectContext对象
+    /// </summary>
+    /// <param name="providerFactory">用于创建数据提供程序类实例的工厂</param>
+    /// <param name="connection">连接</param>
+    /// <param name="transaction">当前连接中已执行命令的事务,没有开启事务时不需要此参数</param>
+    public PostgreSqlExistingConnectionContext(DbProviderFactory providerFactory, DbConnection connection,
+        DbTransaction transaction = null) : base(
+        new PostgreSqlExistingConnectionContextConfiger(
+            new ExistingConnectionSqlExecutor(providerFactory, connection, EDataSource.PostgreSql, transaction)))
+    {
+    }
+}
+
+/// <summary>
+///     使用已存在的PostgreSql连接上下文配置
+/// </summary>
+public class PostgreSqlExistingConnectionContextConfiger : SqlContextConfigurator
+{
+    /// <summary>
+    ///     使用已存在的PostgreSql连接上下文配置。
+    /// </summary>
+    /// <param name="sqlExecutor">Sql语句执行器。</param>
+    /// <param name="enableStructMapping">指示是否启用存储架构映射</param>
+    public PostgreSqlExistingConnectionContextConfiger(ISqlExecutor sqlExecutor, bool enableStructMapping = false) :
+        base(sqlExecutor, enableStructMapping)
+    {
+    }
+
+    /// <summary>
+    ///     使用指定的建模器创建对象数据模型。
+    /// </summary>
+    /// <param name="modelBuilder">对象数据模型建造器</param>
+    protected override void CreateModel(ModelBuilder modelBuilder)
+    {
+        //注册核心模型
+        CoreModelRegister.Regist(modelBuilder);
+    }
+}
--- Src/UnitTest/Obase.Test/ContextUtils.cs
+++ Src/UnitTest/Obase.Test/ContextUtils.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Data.Common;
+using Microsoft.Data.SqlClient;
+using Microsoft.Data.Sqlite;
+using MySql.Data.MySqlClient;
+using Npgsql;
 using Obase.Core;
 using Obase.Providers.Sql;
 using Obase.Test.Infrastructure.Context;
@@ -54,6 +59,35 @@ public static class ContextUtils
                 return new SqliteAddonContext();
             case EDataSource.PostgreSql:
                 return new PostgreSqlAddonContext();
+            case EDataSource.Oracle:
+            case EDataSource.Oledb:
+            case EDataSource.Other:
+            default:
+                throw new ArgumentOutOfRangeException(nameof(dataSource), dataSource, $"暂无{dataSource}对应的对象上下文.");
+        }
+    }
+
+    /// <summary>
+    ///     创建一个新的使用已有连接的上下文
+    /// </summary>
+    /// <param name="connection">已有的连接</param>
+    /// <param name="dataSource">数据源类型</param>
+    /// <param name="transaction">当前连接中已执行命令的事务,如果没有开启过事务,此项可以不传</param>
+    /// <returns></returns>
+    /// <exception cref="ArgumentOutOfRangeException">未定义此类型上下文</exception>
+    public static ObjectContext CreateExistingConnectionContext(DbConnection connection, EDataSource dataSource,
+        DbTransaction transaction = null)
+    {
+        switch (dataSource)
+        {
+            case EDataSource.SqlServer:
+                return new SqlServerExistingConnectionContext(SqlClientFactory.Instance, connection, transaction);
+            case EDataSource.MySql:
+                return new MySqlExistingConnectionContext(MySqlClientFactory.Instance, connection, transaction);
+            case EDataSource.Sqlite:
+                return new SqliteExistingConnectionContext(SqliteFactory.Instance, connection, transaction);
+            case EDataSource.PostgreSql:
+                return new PostgreSqlExistingConnectionContext(NpgsqlFactory.Instance, connection, transaction);
             case EDataSource.Oracle:
             case EDataSource.Oledb:
             case EDataSource.Other:
